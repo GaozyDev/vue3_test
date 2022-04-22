f8d8ec44cf211350b9a49e4e73a1fcd0d8e7456e
GaozyDev <670453367@qq.com>
2022-04-22 14:04:01 +0800

    Teleport瞬移

---
+++ 03_src_ref函数/src/components/Son.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="son">
+    <h3>我是son组件</h3>
+    <Dialog/>
+  </div>
+</template>
+
+<script>
+import Dialog from "@/components/Dialog";
+export default {
+  name: 'Son',
+  components:{Dialog}
+}
+</script>
+<style>
+.son{
+  background-color: orange;
+  padding: 10px;
+}
+</style>
